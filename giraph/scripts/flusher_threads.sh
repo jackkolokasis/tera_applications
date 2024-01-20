b58132f741069be119e45ab3b6fb155f598039b3
Jack Kolokasis <kolokasis@ics.forth.gr>
2024-01-20 11:45:44 +0200

    [feat] Update Giraph scripts to use FlexHeap

--- giraph/scripts/flusher_threads.sh
+++ giraph/scripts/flusher_threads.sh
@@ -2,16 +2,16 @@
 
 reset_stat() {
 	echo 500  | sudo tee /proc/sys/vm/dirty_writeback_centisecs
-	echo 3000 | sudo tee /proc/sys/vm/dirty_expire_centisecs
+	#echo 3000 | sudo tee /proc/sys/vm/dirty_expire_centisecs
 	echo 20   | sudo tee /proc/sys/vm/dirty_ratio
 	echo 10   | sudo tee /proc/sys/vm/dirty_background_ratio
 }
 
 set_stat() {
-	echo 500000 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
-	echo 500000 | sudo tee /proc/sys/vm/dirty_expire_centisecs
-	echo 90		| sudo tee /proc/sys/vm/dirty_ratio
-	echo 90 	| sudo tee /proc/sys/vm/dirty_background_ratio
+	echo 0 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
+	#echo 500000 | sudo tee /proc/sys/vm/dirty_expire_centisecs
+	echo 99		| sudo tee /proc/sys/vm/dirty_ratio
+	echo 99 	| sudo tee /proc/sys/vm/dirty_background_ratio
 }
 
 # Print error/usage script message
