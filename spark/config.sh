0b9c142b7485e57f08f6dcd1760cd2335df2687e
perpap <perpap@pop-os.localdomain>
2024-01-29 00:21:47 +0200

    Switch to openjdk 17 Teraheap for Spark compilation

--- spark/config.sh
+++ spark/config.sh
@@ -12,8 +12,8 @@ export C_INCLUDE_PATH=${TERAHEAP_REPO}/allocator/include/:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=${TERAHEAP_REPO}/allocator/include/:$CPLUS_INCLUDE_PATH
 
 # Set JAVA_HOME to use TeraHeap JVM
-#export JAVA_HOME="${TERAHEAP_REPO}/jdk17u067/build/linux-$CPU_ARCH-server-release/jdk"
-export JAVA_HOME="${TERAHEAP_REPO}/jdk8u345/build/linux-$CPU_ARCH-server-release/jdk"
+export JAVA_HOME="${TERAHEAP_REPO}/jdk17u067/build/linux-$CPU_ARCH-server-release/jdk"
+#export JAVA_HOME="${TERAHEAP_REPO}/jdk8u345/build/linux-$CPU_ARCH-server-release/jdk"
 
 # Set up the path of TeraHeap applications
 TERA_APPS_REPO="/spare/$USER/tera_applications"
