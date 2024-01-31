2edf31d3b7f74a9639c0164a58be8617e8a558aa
Periklis Papoutsis <perpap@ics.forth.gr>
2024-01-31 13:27:37 +0200

    use Local mode

--- spark/spark-bench/conf/env.sh
+++ spark/spark-bench/conf/env.sh
@@ -9,7 +9,8 @@ master="ampere"
 
 # A list of machines where the spark cluster is running
 #MC_LIST="sith4-fast"
-MC_LIST="ampere"
+#MC_LIST="ampere"
+MC_LIST=""
 # Use these inputs for fileio
 #DATA_HDFS=file:///mnt/datasets/SparkBench
 DATA_HDFS=file://$SPARK_DATASETS
@@ -20,7 +21,8 @@ DATASET_DIR="${DATA_HDFS}/dataset"
 SPARK_VERSION=2.3.0
 [ -z "$SPARK_HOME" ] &&  export SPARK_HOME=$TERA_APPLICATIONS_REPO/spark/spark-3.3.0
 
-SPARK_MASTER=spark://${master}:7077
+#SPARK_MASTER=spark://${master}:7077
+SPARK_MASTER=local[2]
 
 SPARK_RPC_ASKTIMEOUT=10000
 # Spark config in environment variable or aruments of spark-submit 
