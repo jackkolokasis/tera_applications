9f6196d6be1b8aa04b4774c65e0f562712e60770
Periklis Papoutsis <perpap@ics.forth.gr>
2024-02-01 12:16:27 +0200

    change path of the maven binary

--- spark/build.sh
+++ spark/build.sh
@@ -88,8 +88,8 @@ benchmark_dependencies() {
     git clone  https://github.com/synhershko/wikixmlj.git >> "${COMPILE_OUT}" 2>&1
 
     cd wikixmlj || exit
-    .${SPARK_DIR}/build/mvn package -Dmaven.test.skip=true >> "${COMPILE_OUT}" 2>&1
-    .${SPARK_DIR}/build/mvn install -Dmaven.test.skip=true >> "${COMPILE_OUT}" 2>&1
+    mvn package -Dmaven.test.skip=true >> "${COMPILE_OUT}" 2>&1
+    mvn install -Dmaven.test.skip=true >> "${COMPILE_OUT}" 2>&1
     cd - >> "${COMPILE_OUT}" 2>&1 || exit
 
     rm -rf ./wikixmlj >> "${COMPILE_OUT}" 2>&1
