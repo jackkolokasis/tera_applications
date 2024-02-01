e511fb0e311a94f44178bc58e5bb0823cc0b66c6
Periklis Papoutsis <perpap@ics.forth.gr>
2024-02-01 12:46:54 +0200

    Set JAVA_HOME to point at the Teraheap's openjdk 8 for Spark compilation

--- spark/config.sh
+++ spark/config.sh
@@ -11,9 +11,9 @@ export PATH=${TERAHEAP_REPO}/allocator/include:$PATH
 export C_INCLUDE_PATH=${TERAHEAP_REPO}/allocator/include:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=${TERAHEAP_REPO}/allocator/include:$CPLUS_INCLUDE_PATH
 
-# Set JAVA_HOME to use TeraHeap JVM
+# Set JAVA_HOME to use TeraHeap openjdk 8 JVM for Spark compilation
 #export JAVA_HOME="${TERAHEAP_REPO}/jdk17u067/build/linux-$CPU_ARCH-server-release/jdk"
-export JAVA_HOME="${TERAHEAP_REPO}/jdk8u345/build/linux-$CPU_ARCH-server-release/jdk"
+export JAVA_HOME="${TERAHEAP_REPO}/jdk8u345/build/linux-$CPU_ARCH-normal-server-release/jdk"
 #export JAVA_HOME=/spare/$USER/openjdk/jdk8u402-b06
 # Set up the path of TeraHeap applications
 #TERA_APPLICATIONS_REPO=$TERA_APPLICATIONS_HOME
