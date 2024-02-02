7271f716f3e5cf68129f0f3faf896efe7c5b1ff4
Periklis Papoutsis <perpap@ampere.cluster.ics.forth.gr>
2024-02-02 15:06:50 +0100

    Dont use environment variables for paths; cgroup ignores them

--- spark/spark-bench/conf/env.sh
+++ spark/spark-bench/conf/env.sh
@@ -4,26 +4,21 @@
 USER=$(whoami)
 
 # Spark Master
-#master="sith4-fast"
 master="ampere"
 
 # A list of machines where the spark cluster is running
-#MC_LIST="sith4-fast"
-MC_LIST=ampere
-#MC_LIST=""
+MC_LIST="ampere"
 # Use these inputs for fileio
-#DATA_HDFS=file:///mnt/datasets/SparkBench
-DATA_HDFS="file://$MOUNT_POINT_BENCHMARK_DATASETS/SparkBench"
+DATA_HDFS=file:///mnt/datasets/SparkBench
 # Local dataset optional
 DATASET_DIR="${DATA_HDFS}/dataset"
 
 # Use this when run on Spark 2.3.0-kolokasis
 #Use the SPARK_VERSION environment variable
 #SPARK_VERSION=3.3.0
-[ -z "$SPARK_HOME" ] &&  export SPARK_HOME=$TERA_APPLICATIONS_REPO/spark/spark-$SPARK_VERSION
+[ -z "$SPARK_HOME" ] &&  export SPARK_HOME=/spare/perpap/tera_applications/spark/spark-3.3.0
 
 SPARK_MASTER=spark://${master}:7077
-#SPARK_MASTER=local[2]
 
 SPARK_RPC_ASKTIMEOUT=10000
 # Spark config in environment variable or aruments of spark-submit 
@@ -32,9 +27,9 @@ SPARK_RDD_COMPRESS=false
 #SPARK_IO_COMPRESSION_CODEC=lzf
 
 # Spark options in system.property or arguments of spark-submit 
-SPARK_EXECUTOR_MEMORY=1200g
+SPARK_EXECUTOR_MEMORY=38g
 SPARK_EXECUTOR_INSTANCES=1
-SPARK_EXECUTOR_CORES=16
+SPARK_EXECUTOR_CORES=8
 
 # Storage levels, see :
 STORAGE_LEVEL=MEMORY_ONLY
