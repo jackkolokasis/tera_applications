8eab16317ce98770ea61931cb0b20b809e04cd28
Jack Kolokasis <kolokasis@ics.forth.gr>
2024-04-13 16:45:45 +0300

    [fix] Remove static paths

--- spark/scripts/run_gen_dataset_tpcds.sh
+++ spark/scripts/run_gen_dataset_tpcds.sh
@@ -21,7 +21,7 @@
   --conf spark.executor.memory="${H1_SIZE[0]}"g \
   "${SPARK_DIR}"/sql/core/target/spark-sql_2.12-3.3.0-tests.jar \
   --master spark://sith4-fast:7077 \
-  --dsdgenDir /opt/carvguest/asplos23_ae/tera_applications/spark/spark-tpcds/build/resources/main/binaries/Linux/x86_64/ \
+  --dsdgenDir "${BENCH_DIR}"/spark/spark-tpcds/build/resources/main/binaries/Linux/x86_64/ \
   --location "${DATA_HDFS}"/tpcds \
   --scaleFactor 200 \
   --numPartitions 256
