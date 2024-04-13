d98f342c2e5ae1d4b06a424d36db258786563531
Jack Kolokasis <kolokasis@ics.forth.gr>
2024-04-13 14:59:25 +0300

    [fix] Fix the execution time parsing in TPC-DS
    - Before we account the average time of each query which this is wrong
    - Benchmark runs each query for two iterations, so we need to measure
      the total time of the two iterations. Our benchmark accounts the GC
      time for the two iterations.

--- spark/scripts/run_tpcds.sh
+++ spark/scripts/run_tpcds.sh
@@ -23,12 +23,18 @@ QUERY_NAME=$3
   --conf spark.executor.instances="${NUM_EXECUTORS[0]}" \
   --conf spark.executor.cores="${EXEC_CORES[0]}" \
   --conf spark.executor.memory="${HEAP_SIZE}"g \
-  --conf spark.sql.shuffle.partitions=256 \
+  --conf spark.sql.shuffle.partitions=32 \
   --jars "${SPARK_DIR}"/core/target/spark-core_2.12-3.3.0-tests.jar,"${SPARK_DIR}"/sql/catalyst/target/spark-catalyst_2.12-3.3.0-tests.jar \
   --class org.apache.spark.sql.execution.benchmark.TPCDSQueryBenchmark \
   "${SPARK_DIR}"/sql/core/target/spark-sql_2.12-3.3.0-tests.jar \
   --data-location "${DATA_HDFS}"/tpcds \
   --query-filter "${QUERY_NAME}" > "${RUN_DIR}"/tmp_out.txt 2>&1
 
-duration=$(grep -E "^${QUERY_NAME}\s+" "${RUN_DIR}"/tmp_out.txt | awk '{print $3}')
-echo ",,${duration}" >> "${RUN_DIR}"/total_time.txt
+# Search for the line containing the specified text and extract the numeric part using regex
+duration=$(grep -Eo "Stopped after [0-9]+ iterations, ([0-9]+) ms" "${RUN_DIR}"/tmp_out.txt \
+  | grep -Eo "[0-9]+" | tail -n 1)
+
+# Convert from milliseconds to seconds
+time_in_sec=$(echo "$duration / 1000" | bc)
+
+echo ",,${time_in_sec}" >> "${RUN_DIR}"/total_time.txt
