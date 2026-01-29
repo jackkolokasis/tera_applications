fa8f2230b952a18f36d3748d38c1de3d4be01551
Periklis Papoutsis <perpap@ics.forth.gr>
2026-01-29 09:02:21 +0000

    run teraheap and native in batch

---
+++ spark/scripts/run_batch_teraheap_and_native.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+./run_batch.sh -t /spare/s0/perpap/teraheap -g amperesudo -m ampere -s ampere -f /spare/s2/perpap/fmap -p /spare/s2/perpap/spark -d /spare/s1/perpap/datasets -r /spare/s2/perpap/spark_results -e teraheap_g1 -j /spare/s0/perpap/teraheap/jdk17/build/linux-aarch64-server-release/jdk -t 256 -w AsyncWritePolicy -l asplos_config.sh -c -o
+
+./run_batch.sh -t /spare/s0/perpap/teraheap -g amperesudo -m ampere -s ampere -f /spare/s2/perpap/fmap -p /spare/s2/perpap/spark -d /spare/s1/perpap/datasets -r /spare/s2/perpap/spark_results -e native_g1 -j /spare/s0/perpap/teraheap/jdk17/build/linux-aarch64-server-release/jdk -t 256 -w AsyncWritePolicy -l asplos_config.sh -c -o
